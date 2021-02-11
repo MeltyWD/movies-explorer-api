18f6d3248379056b66fef31f0ba2db1b7b938020
Melty <Meltygd@gmail.com>
2021-02-11 14:18:40 +0900

    исправление линтера

--- app.js
+++ app.js
@@ -68,7 +68,4 @@ app.use((err, req, res, next) => {
   next();
 });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT, () => {});
