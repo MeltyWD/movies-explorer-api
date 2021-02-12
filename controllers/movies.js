10058bf19b2194bef391f91e6a968d76a15edf03
Melty <Meltygd@gmail.com>
2021-02-12 16:49:56 +0900

    переработан контроллер movie

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,6 +2,7 @@ const Movie = require('../models/movie');
 const NotFoundError = require('../errors/not-found-error');
 const BadRequest = require('../errors/bad-request-error');
 const ForbiddenError = require('../errors/forbidden-error');
+const ConflictError = require('../errors/conflict-error');
 
 module.exports.getFilms = async (req, res, next) => {
   try {
@@ -43,6 +44,10 @@ module.exports.createFilm = async (req, res, next) => {
     });
     res.send(movieElem);
   } catch (err) {
+    if (err.code === 11000) {
+      const conflictError = new ConflictError('Фильм с таким Id уже существует');
+      next(conflictError);
+    }
     if (err.name === 'ValidationError') {
       const badRequestError = new BadRequest('Переданы некорректные данные');
       next(badRequestError);
@@ -54,13 +59,12 @@ module.exports.createFilm = async (req, res, next) => {
 module.exports.deleteFilm = async (req, res, next) => {
   try {
     const { movieId } = req.params;
-    const findMovie = Movie.findById(movieId)
+    const findMovie = await Movie.findById(movieId).select('+owner')
       .orFail(new NotFoundError('Фильм не найден'));
-
     if (findMovie.owner.toString() !== req.user._id) {
       throw new ForbiddenError('Нет прав на удаление');
     } else {
-      const deleteElem = await Movie.findByIdAndDelete(movieId);
+      const deleteElem = await Movie.findByIdAndDelete(movieId).select('-owner');
       res.send(deleteElem);
     }
   } catch (err) {
